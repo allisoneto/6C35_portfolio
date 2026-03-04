b7647917073f67fa7c48d4e8a0e80d9bdf3ba583
allisoneto <116852822+allisoneto@users.noreply.github.com>
2026-03-03 21:36:41 -0500

    attempt to fix github link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,11 +23,11 @@
     <ul>
         {#each pages as p}
         <li>
-            <a href={base + p.url}
+            <a href={p.url.startsWith("http") ? p.url : base + p.url}
                 class:current={p.url === "/" // is this link the home page?
                 ? $page.url.pathname === (base + "/") // if yes - set current = true if the path name matches. Else, set current = true if the path name starts correctly
                 : $page.url.pathname.startsWith(base + p.url)}
-                target={p.url.startsWith("http") ? "_blank": null}
+                target={p.url.startsWith("http") ? "_blank" : null}
             >
                 {p.title}
             </a>
